d2247e8b3010cd1c0fb12fd7b2e56582a147b99c
Yahya Brital <30161696+Yahya-bri@users.noreply.github.com>
2025-01-07 16:39:01 +0100

    feat: update environment configuration for development and production setups

--- user-service/config.py
+++ user-service/config.py
@@ -6,22 +6,26 @@ dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
 if os.path.exists(dotenv_path):
     load_dotenv(dotenv_path)
 
+DATABASE_URL_DEV = os.getenv('DATABASE_URL_DEV')
+DATABASE_URL_PROD = os.getenv('DATABASE_URL_PROD')
+SECRET_KEY = os.getenv('SECRET_KEY')
+
 
 class Config:
-    SECRET_KEY = "mrfrIMEngCl0pAKqIIBS_g"
+    SECRET_KEY = SECRET_KEY
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
 class DevelopmentConfig(Config):
     ENV = "development"
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://cloudacademy:pfm_2020@host.docker.internal:3306/user_dev'
+    SQLALCHEMY_DATABASE_URI = DATABASE_URL_DEV
     SQLALCHEMY_ECHO = True
 
 
 class ProductionConfig(Config):
     ENV = "production"
     DEBUG = False
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://cloudacademy:pfm_2020@user-db:3306/user'
+    SQLALCHEMY_DATABASE_URI = DATABASE_URL_PROD
     SQLALCHEMY_ECHO = False
 
