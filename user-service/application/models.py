b8d0254128ac485d32565d76c4338c530abf2286
Yahya Brital <30161696+Yahya-bri@users.noreply.github.com>
2025-01-08 15:01:17 +0100

    refactor: simplify user-role and permission-role group associations in models

--- user-service/application/models.py
+++ user-service/application/models.py
@@ -3,18 +3,21 @@ from . import db
 from datetime import datetime
 from flask_login import UserMixin
 from passlib.hash import sha256_crypt
-from sqlalchemy_serializer import SerializerMixin
 
 
-class UserRoleGroup(db.Model, SerializerMixin):
-    __tablename__ = 'user_role_group'
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), primary_key=True)
-    role_group_id = db.Column(db.Integer, db.ForeignKey('role_group.id'), primary_key=True)
 
-class PermissionRoleGroup(db.Model, SerializerMixin):
-    __tablename__ = 'permission_role_group'
-    permission_id = db.Column(db.Integer, db.ForeignKey('permission.id'), primary_key=True)
-    role_group_id = db.Column(db.Integer, db.ForeignKey('role_group.id'), primary_key=True)
+# Association table for User and RoleGroup
+user_role_group = db.Table('user_role_group',
+    db.Column('user_id', db.Integer, db.ForeignKey('user.id'), primary_key=True),
+    db.Column('role_group_id', db.Integer, db.ForeignKey('role_group.id'), primary_key=True)
+)
+
+
+# Association table for Permission and RoleGroup
+permission_role_group = db.Table('permission_role_group',
+    db.Column('permission_id', db.Integer, db.ForeignKey('permission.id'), primary_key=True),
+    db.Column('role_group_id', db.Integer, db.ForeignKey('role_group.id'), primary_key=True)
+)
 
 
 class User(UserMixin, db.Model):
@@ -27,12 +30,12 @@ class User(UserMixin, db.Model):
     is_admin = db.Column(db.Boolean, default=False)
     authenticated = db.Column(db.Boolean, default=False)
     api_key = db.Column(db.String(255), unique=True, nullable=True)
-    date_added = db.Column(db.DateTime, default=datetime.utcnow)
-    date_updated = db.Column(db.DateTime, onupdate=datetime.utcnow)
-    role_groups = db.relationship('RoleGroup', secondary=user_role_group, backref=db.backref('users', lazy=True))
+    date_added = db.Column(db.DateTime, default=lambda: datetime.now(datetime.timezone.utc))
+    date_updated = db.Column(db.DateTime, onupdate=lambda: datetime.now(datetime.timezone.utc))
+    role_groups = db.relationship('RoleGroup', secondary=user_role_group, backref=db.backref('user_role_groups', lazy=True))
 
     def encode_api_key(self):
-        self.api_key = sha256_crypt.hash(self.username + str(datetime.utcnow))
+        self.api_key = sha256_crypt.hash(self.username + str(datetime.now(datetime.timezone.utc)))
 
     def encode_password(self):
         self.password = sha256_crypt.hash(self.password)
@@ -57,8 +60,8 @@ class RoleGroup(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255), unique=True, nullable=False)
     description = db.Column(db.String(255), nullable=True)
-    users = db.relationship('User', secondary=user_role_group, backref=db.backref('role_groups', lazy=True))
-    permissions = db.relationship('Permission', secondary=permission_role_group, backref=db.backref('role_groups', lazy=True))
+    users = db.relationship('User', secondary=user_role_group, backref=db.backref('user_role_groups', lazy=True))
+    permissions = db.relationship('Permission', secondary=permission_role_group, backref=db.backref('permission_role_groups', lazy=True))
 
     def __repr__(self):
         return '<RoleGroup %r>' % (self.name)
