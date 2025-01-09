fc562d244687b741c47d37efeaf835b9b2ad24bf
Yahya Brital <30161696+Yahya-bri@users.noreply.github.com>
2025-01-09 11:45:04 +0100

    feat: enhance admin functionality with password hashing and add admin page link

--- user-service/application/AdminView.py
+++ user-service/application/AdminView.py
@@ -1,17 +1,23 @@
 from flask_admin.contrib.sqla import ModelView
 from flask_login import current_user
 from flask import redirect, url_for
+from passlib.hash import sha256_crypt
 
 class AdminModelView(ModelView):
     def is_accessible(self):
         return current_user.is_authenticated and current_user.is_admin
 
     def inaccessible_callback(self, name, **kwargs):
-        return redirect(url_for('login'))
+        return redirect(url_for('user_app.login'))
 
 class UserAdmin(AdminModelView):
     column_list = ('id', 'username', 'email', 'first_name', 'last_name', 'is_admin', 'date_added', 'date_updated')
     form_columns = ('username', 'email', 'first_name', 'last_name', 'password', 'is_admin', 'role_groups')
+
+    def on_model_change(self, form, model, is_created):
+        if form.password.data:
+            model.password = sha256_crypt.hash(str(form.password.data))
+        return super(UserAdmin, self).on_model_change(form, model, is_created)
     
     
 class RoleGrpAdmin(AdminModelView):
